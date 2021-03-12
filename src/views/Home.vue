47409756912618dca915ddc1a7463be2ecc2df6e
titangene <titangene.tw@gmail.com>
2021-03-13 00:01:08 +0800

    feat: +1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="@/assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App (develop)" />
+    <HelloWorld msg="Welcome to Your Vue.js App (develop + 1)" />
   </div>
 </template>
 
