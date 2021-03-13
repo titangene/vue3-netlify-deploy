106c32916b0df9fbc0d4360464318f53be715884
titangene <titangene.tw@gmail.com>
2021-03-13 10:52:48 +0800

    feat: lock + 2

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="@/assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App (main lock + 1)" />
+    <HelloWorld msg="Welcome to Your Vue.js App (main lock + 2)" />
     <p>{{ apiBaseURL }}</p>
   </div>
 </template>
